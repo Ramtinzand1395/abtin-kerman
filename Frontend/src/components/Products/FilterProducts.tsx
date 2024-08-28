8548c9ec290bc67348d2daa2c6d98e19ecc1e9d3
ramtin.zand <ramtinzand6@gmail.com>
2024-08-28 11:58:35 +0330

    google auth

--- Frontend/src/components/Products/FilterProducts.tsx
+++ Frontend/src/components/Products/FilterProducts.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import DropDown from "../utils/DropDown";
+import { FiEdit } from "react-icons/fi";
 
 const FilterProducts: React.FC = () => {
+  const DropdownIthem = {
+    text1: "مرتب سازی بر اساس",
+    icon1: FiEdit,
+    link1: `/dashboard`,
+    text2: "ویرایش حساب کاربری",
+    icon2: FiEdit,
+    text3: "ویرایش حساب کاربری",
+    icon3: FiEdit,
+    text4: "ویرایش حساب کاربری",
+    icon4: FiEdit,
+    title: "خوش آمدید",
+  };
   return (
     <div className="flex items-center justify-between">
       مرتب سازی بر اساس :
-      <DropDown />
+      <DropDown DropdownIthem={DropdownIthem}/>
     </div>
   );
 };
