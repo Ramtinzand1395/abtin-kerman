e619af14b017ca3674e10ba8347f78cc86386156
ramtin.zand <ramtinzand6@gmail.com>
2024-09-15 18:39:53 +0330

    homepagechange

--- Frontend/src/components/Products/FilterProducts.tsx
+++ Frontend/src/components/Products/FilterProducts.tsx
@@ -1,25 +1,26 @@
 import React from "react";
-import DropDown from "../utils/DropDown";
-import { FiEdit } from "react-icons/fi";
+// import DropDown from "../utils/DropDown";
+// import { FiEdit } from "react-icons/fi";
 
 const FilterProducts: React.FC = () => {
-  const DropdownIthem = {
-    text1: "مرتب سازی بر اساس",
-    icon1: FiEdit,
-    link1: `/dashboard`,
-    text2: "ویرایش حساب کاربری",
-    icon2: FiEdit,
-    text3: "ویرایش حساب کاربری",
-    icon3: FiEdit,
-    text4: "ویرایش حساب کاربری",
-    icon4: FiEdit,
-    title: "مرتب سازی بر اساس",
-    link4:"salam"
-  };
+  // const DropdownIthem = {
+  //   text1: "مرتب سازی بر اساس",
+  //   icon1: FiEdit,
+  //   link1: `/dashboard`,
+  //   text2: "ویرایش حساب کاربری",
+  //   icon2: FiEdit,
+  //   text3: "ویرایش حساب کاربری",
+  //   icon3: FiEdit,
+  //   text4: "ویرایش حساب کاربری",
+  //   icon4: FiEdit,
+  //   title: "مرتب سازی بر اساس",
+  //   link4:"salam"
+  // };
   return (
     <div className="flex items-center justify-between">
       مرتب سازی بر اساس :
-      <DropDown DropdownIthem={DropdownIthem}/>
+      {/* مشکا دراپ دان بعدا درست بشه */}
+      {/* <DropDown DropdownIthem={DropdownIthem}/> */}
     </div>
   );
 };
