075a00a19e1fd592b4d029f7f6031ac017a382fd
ramtin.zand <ramtinzand6@gmail.com>
2024-12-21 12:44:43 -0400

    pwa4

--- Frontend/src/components/pages/home/hero/Hero.tsx
+++ Frontend/src/components/pages/home/hero/Hero.tsx
@@ -4,17 +4,11 @@ import { Navigation, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "./hero.css";  // Ensure this only includes necessary styles
 
-// import test from "../../../../assets/Hero/ps5-Hero.jpg";
-// import test2 from "../../../../assets/Hero/redDead-Hero.jpg";
-// import test3 from "../../../../assets/Hero/gta-Hero.jpg";
 import test3 from "../../../../assets/Hero/488598f9-6aca-4f29-8d85-1c6a3281a47c.webp";
 import Stest3 from "../../../../assets/Hero/as-requested-the-individual-layers-to-the-gta-vi-key-art-v0-ig1e88inlh4c1-copy1.webp";
-
 import test2 from "../../../../assets/Hero/Kerfin7_NEA_2337.webp";
 import Stest2 from "../../../../assets/Hero/as-requested-the-individual-layers-to-the-gta-vi-key-art-v0-ig1e88inlh4c1New.webp";
-
 import test from "../../../../assets/Hero/d65b8f21-ba9f-421a-9daf-c5c20446d502.webp";
-// import Stest from "../../../../assets/Hero/as-requested-the-individual-layers-to-the-gta-vi-key-art-v0-ig1e88inlh4c1new3.png";
 
 import SliderpageTest from "../SliderpageTest";
 
