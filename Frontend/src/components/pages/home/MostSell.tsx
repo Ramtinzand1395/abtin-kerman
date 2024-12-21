3ed7a5d326ace3aa42001a9a40b17cd4430d3156
ramtin.zand <ramtinzand6@gmail.com>
2024-12-21 13:49:41 -0400

    pwa5

--- Frontend/src/components/pages/home/MostSell.tsx
+++ Frontend/src/components/pages/home/MostSell.tsx
@@ -8,8 +8,6 @@ import hover5 from "../../../assets/Hover/Spider-man_Hover.webp";
 import hover6 from "../../../assets/Hover/Spider-man.webp";
 import hover7 from "../../../assets/Hover/Wukong_hover.webp";
 import hover8 from "../../../assets/Hover/Wukong_Correct.webp";
-
-import Animations from "../../utils/Animations";
 import LeftAnimation from "../../utils/LeftAnimation";
 
 const MostSell: React.FC = () => {
@@ -41,14 +39,52 @@ const MostSell: React.FC = () => {
   ];
   return (
     <div>
-      <Animations>
+      {/* <Animations>
         <div className="flex items-center mt-10 mb-2">
           <h2 className="font-bold text-lg lg:text-2xl whitespace-nowrap ml-5">
             بازی های پر فروش
           </h2>{" "}
-          <div className="w-full h-[8px] rounded-3xl bg-primary"></div>{" "}
+          </div>
+          </Animations> */}
+      <div className="w-full h-[8px] rounded-3xl bg-primary"></div>{" "}
+      <LeftAnimation>
+        <div className="my-5">
+
+        <h3 className="font-tanha text-primary my-4">
+          خوش آمدید به کرمان آتاری
+        </h3>
+        <p>
+          در "کرمان آتاری"، تجربه‌ای منحصربه‌فرد از خرید و فروش بازی‌های
+          پلی‌استیشن و کنسول‌های سونی را برای شما فراهم کرده‌ایم. هدف ما ارائه
+          محصولات با کیفیت بالا، قیمت مناسب، و گارانتی معتبر است تا هر مشتری با
+          خیالی آسوده از ما خرید کند.
+        </p>
         </div>
-      </Animations>
+        <div className="">
+        <h3 className="font-tanha text-primary my-4">
+          درباره ما
+
+          </h3>
+        <p>
+          درباره ما کرمان آتاری با بیش از نیم‌قرن سابقه (از سال 1340) در زمینه
+          خرید و فروش بازی‌های اکانتی و دستگاه‌های پلی‌استیشن فعالیت می‌کند. ما
+          مفتخریم که همواره به رضایت مشتریان، سرعت در ارائه خدمات، و کیفیت برتر
+          محصولات خود متعهد بوده‌ایم.
+        </p>
+        </div>
+        <div className="">
+        <h3 className="font-tanha text-primary my-4">
+          چرا کرمان آتاری؟
+
+          </h3>
+          <p>
+          کیفیت بالا: تمامی محصولات ما اصل و با گارانتی معتبر ارائه می‌شوند.
+سرعت و قیمت مناسب: خریدی آسان و سریع همراه با قیمتی رقابتی.
+پشتیبانی دوستانه: تیم ما همواره آماده پاسخ‌گویی به سوالات شماست.
+
+          </p>
+        </div>
+      </LeftAnimation>
       <LeftAnimation>
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 my-10 ">
           {hovercars?.map((card) => (
